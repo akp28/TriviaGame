69081f5bd095b562f37ae0f30541a51b96bd3aaa
akp28 <ajaypaladugu@gmail.com>
2018-10-20 19:38:54 -0700

    Changed questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,56 +2,72 @@ var number = 10;
 var intervalId;
 var raCounter = 0;
 var waCounter = 0;
+var clockRunning = false;
 
 function run() {
-    clearInterval(intervalId);
+    if (!clockRunning) {
+    // clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-}
-//  The decrement function.
-function decrement() {
-//  Decrease number by one.
+    clockRunning = true;
+    }
+  }
+
+  //  The decrement function.
+  function decrement() {
+
+    //  Decrease number by one.
     if(number > 0){
         number--;
     }
     
+
     //  Show the number in the #show-number tag.
-    $("#show-number").html("<h2>" + number + "</h2>");
-    renderQuestion();
+    $(".show-timer").html("<h1> Time Remaining: " + number + "</h1>");
+
+
     //  Once number hits zero...
     if (number === 0) {
-        //  ...run the stop function.
-        // stop();
-        //  Alert the user that time is up.
-        alert("Time Up!");
-        number=10;
+
+      //  ...run the stop function.
+      stop();
+
+      //  Alert the user that time is up.
+      alert("Time Up!");
+      questionIndex ++;
+      waCounter ++;
+      renderQuestion();
+    //   run();
     }
-}
-// function stop() {
-//     clearInterval(intervalId,0);
-//     console.log(intervalId);
-// } 
+  }
+
+  //  The stop function
+  function stop() {
+
+    //  Clears our intervalId
+    //  We just pass the name of the interval
+    //  to the clearInterval function.
+    clearInterval(intervalId);
+    clockRunning = false;
+    number = 10;
+  }
+
+  //  Execute the run function.
+//   run();
 
 var questions = [
-    { question: "What’s Dory’s very first line in the movie?",
-      answer:"Look out",
-      choice: ["Look out","Hi I m dory","Just keep swimming","Hey there" ],
-      photo: "https://lumiere-a.akamaihd.net/v1/images/q1_doryswimmingtowardscamrea_findingnemo_3dc9f828.jpeg?region=0%2C0%2C1024%2C338" 
+    { question: "How many legs does a spider have?",
+      answer:"Eight",
+      choice: ["Ten","Eight","Five","Six"],
     },
-    { question: "What color are Dory’s eyes?",
-      answer: "Purple",
-      choice: ["Blue", "Yellow", "Green", "Purple"],
-      photo: "https://lumiere-a.akamaihd.net/v1/images/q2_dorycryingtalkingtomarlin_findingnemo_9690f45a.jpeg?region=0%2C0%2C1024%2C338"
+    { question: "What is the name of the pirate in Peter Pan?",
+      answer: "Captain Hook",
+      choice: ["Captain Hook", "Captain Cook", "Captain Jim", "Captain Joe"],
     },
-    { question: "What does Dory think this shape is?",
-      answer: "clam",
-      choice: ["Octopus","clam","Spider","Tuna"],
-      photo: "https://lumiere-a.akamaihd.net/v1/images/q6_fishmakingoctopussign_findingnemo_d61baf9f.jpeg?region=0%2C0%2C1024%2C338"
+    { question: "How many rings make up the symbol of the Olympic Games?",
+      answer: "Five",
+      choice: ["Six","Five","Four","Three"],
     }
-];
-
-// var ans1 =  ["Look out","Hi I m dory","Just keep swimming","Hey there" ];
-// var ans2 = ["Blue", "Yellow", "Green", "purple"];
-// var ans3 = ["Octopus","clam","Spider","Tuna"];           
+];         
               
               
 var cList =$("ul.answers");
@@ -63,7 +79,7 @@ function answerOption(value) {
         var aaa = $('<a/>')
             .text(value)
             .appendTo(li);
-    console.log(value);
+    // console.log(value);
 }
 
 // We start the game with a score of 0.
@@ -81,6 +97,7 @@ function renderQuestion() {
 //  var len = questions.length;
   console.log("ques len " + questions.length);
   if (questionIndex <= (questions.length - 1)) {
+    run();
     if (questionIndex === 0){
         console.log("if");
         $(".question").html(questions[questionIndex].question);
@@ -102,9 +119,11 @@ function renderQuestion() {
 }
 
 function showStats(){
-    // $(".cardbody").empty();
-    $("ul").empty();
-    $(".finalScore").html("You Got " +raCounter+ " correct of total 3 questions ")
+    $(".question-block").empty();
+    // $("ul").empty();
+    $("show-timer").empty();
+    var totalQuestions = questions.length;
+    $(".finalScore").html("You Got " +raCounter+ " correct of total " +totalQuestions +  " questions ")
 };
 
 $(document).on('click','.list',answerClick);
@@ -112,27 +131,34 @@ $(document).on('click','.list',answerClick);
 
 function answerClick(){
         var input = $(this).text();
-        console.log("click " + input);
+        // console.log("click " + input);
         var ans = questions[questionIndex].answer
-        console.log("answers " + ans);
+        // console.log("answers " + ans);
         if(input.trim() == ans.trim()){
             console.log("true");
-            alert("correct answer");
+            // alert("correct answer");
+            stop();
             raCounter ++;
             questionIndex ++;
+            $(".list").text(" ");
+            // $("ul").html("You selected choice is Right!!");
+            $("ul").append("<li>You selected choice is Right!! </li>");  
             renderQuestion();
+            // run();
         }else{
-            console.log("false");
-            alert("wrong answer. " + ans + " is right answer");
+            // console.log("false");
+            // alert("wrong answer. " + ans + " is right answer");
+            stop();
             waCounter ++;
             questionIndex ++;
+            $("ul").append("<li>You selected choice is Wrong!! </li>");
             renderQuestion();
             // return;
         }
-}
+};
 
 
 renderQuestion();
 // showStats();
 // score();
-// run();
+//  run();
